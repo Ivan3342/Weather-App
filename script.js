7198b2f052b623e6e1e5be77b7debe4b8413f41e
Ivan Stojadinovic <stojadinovicivan0@gmail.com>
2025-11-04 19:15:48 +0100

    added the current temp box and some style

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const appContainer = document.querySelector(".app-container");
 const API_KEY = "4858a15fb45b40c2b08234623251410";
 const BASE_URL = "http://api.weatherapi.com/v1";
 const API_METHODS = {
@@ -6,8 +7,23 @@ const API_METHODS = {
     search: "/search.json"
 }
 
-console.log(`${BASE_URL}${API_METHODS.current}?key=${API_KEY}`);
+fetch(`${BASE_URL}${API_METHODS.current}?key=${API_KEY}&q=auto:ip`)
+.then(rawData => rawData.json())
+.then(data => {
+    appContainer.innerHTML = `
+        <div class="current-weather">
+            <div>
+                <h1>Current Weather</h1>
+                <p>${data.location.localtime}</p>
+            </div>
+            <div>
+                <span>
+                    <h1>${data.current.temp_c}&deg;C</h1>
+                    <img src="${data.current.condition.icon}">
+                </span>
+                <h2>${data.current.condition.text}</h2>
+            </div>
+        </div>
+    `
+})
 
-fetch(`${BASE_URL}${API_METHODS.current}?key=${API_KEY}&q=auto:ip`).then(
-
-);
